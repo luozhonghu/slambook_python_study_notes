84ba47abe8a9a1475aca49a7daab33383e612025
luozhonghu <123706477+luozhonghu@users.noreply.github.com>
2025-02-13 16:32:00 +0800

    Add files via upload

---
+++ ch2/HelloSLAM.py
@@ -0,0 +1,8 @@
+# 文件名: libHelloSLAM.py
+
+def print_hello():
+    print("Hello SLAM")
+
+# 如果这个脚本被直接运行（而不是被导入），执行 print_hello 函数
+if __name__ == "__main__":
+    print_hello()
---
+++ ch2/useHello.py
@@ -0,0 +1,6 @@
+from sub_file.HelloSLAM import print_hello
+
+# 如果这个脚本被直接运行（而不是被导入），执行 print_hello 函数
+if __name__ == "__main__":
+    print_hello()
+
